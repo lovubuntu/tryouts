77352972de58df562ddf0e175530cb516188029e
PrabuK <meetprabu88@gmail.com>
2016-12-04 13:09:21 +0530

    Lovubuntu | Common place with default arguments and guarded by guard clause

--- elixir/exercise_6_default_params.exs
+++ elixir/exercise_6_default_params.exs
@@ -6,3 +6,22 @@ end
 
 DefaultParams.print_args(24)
 DefaultParams.print_args(12, 10, 23, 42)
+
+defmodule ConfusionLessDefault do
+  # The same default argument gets applied to all functions and guard clause will do the rest
+  def print_args(p1, p2 \\"world")
+
+  def print_args(p1, p2) when is_bitstring p2 do
+    IO.puts "Hello #{p1} welcome to #{p2}"
+  end
+
+  def print_args(p1, p2) when is_number(p2) do
+    IO.puts "Looks like you passed a number as the second argument"
+    IO.inspect [p1, p2]
+  end
+end
+
+ConfusionLessDefault.print_args("Lovubuntu")
+ConfusionLessDefault.print_args("Prince", 234)
+ConfusionLessDefault.print_args("Prince", "Green")
+ConfusionLessDefault.print_args(42)
