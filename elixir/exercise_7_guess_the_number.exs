eaa449b1e2837a2503108c12d83f21a2a0e40963
PrabuK <meetprabu88@gmail.com>
2016-12-05 19:56:49 +0530

    Lovubuntu | I used it Tesla! I am a magician now :)

--- elixir/exercise_7_guess_the_number.exs
+++ elixir/exercise_7_guess_the_number.exs
@@ -1,30 +1,21 @@
 defmodule Magician do
   def guess(actual_number, range) do
-    check_number(actual_number, mid_of_range(range), range)
+    guess = mid_of_range(range)
+    IO.puts "Is it #{guess}?"
+    check_number(actual_number, guess, range)
   end
-  #
-  # def check_number(actual_number, range) when mid_of_range(range) == actual_number, do: actual_number
-  # def check_number(actual_number, range) when mid_of_range(range) > actual_number do
-  #   start..finish = range
-  #   new_end = mid_of_range(range)-1
-  #   check_number(actual_number, start..new_end)
-  # end
-  # def check_number(actual_number, range) when mid_of_range(range) < actual_number do
-  #   start..finish = range
-  #   new_start = mid_of_range(range)+1
-  #   check_number(actual_number, new_start..finish)
-  # end
 
-  def check_number(actual, guess, _) when actual == guess, do: IO.puts actual
-  def check_number(actual, guess, start..finish) when guess > actual do
-    IO.puts "Is it #{guess}"
+  def check_number(actual, guess, _) when actual == guess, do: actual
+
+  def check_number(actual, guess, start.._) when guess > actual do
     guess(actual, start..guess-1)
   end
-  def check_number(actual, guess, start..finish) when guess < actual do
-    IO.puts "closer to #{guess}?"
+
+  def check_number(actual, guess, _..finish) when guess < actual do
     guess(actual, guess+1..finish)
   end
-  def mid_of_range(a..b), do: div(a+b,2)
+
+  def mid_of_range(start..finish), do: div(start + finish, 2)
 end
 
-Magician.guess(123, 100..200)
+IO.puts Magician.guess(123, 100..200)
